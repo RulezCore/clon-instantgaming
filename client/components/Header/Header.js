af1bd2a9501327b72d80b44f8c7c516f0c7a9c18
Daniel <ixrulez69@gmail.com>
2021-03-15 20:25:11 +0100

    client

---
+++ client/components/Header/Header.js
@@ -0,0 +1,11 @@
+import TopBar from "./TopBar";
+import MenuWeb from "./Menu";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <TopBar />
+      <MenuWeb />
+    </div>
+  );
+}
---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+// import { Button, Icon } from "semantic-ui-react";
+import BasicLayout from '../layouts/BasicLayout'
+
+
+export default function Home() {
+  return (
+    <BasicLayout>
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </BasicLayout>
+  );
+}
