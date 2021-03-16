c8ef0acaa10952d07051bdea2f2420be1e603524
RulezCore <ixrulez69@gmail.com>
2021-03-16 22:53:33 +0100

    porsi

--- client/components/Auth/RegisterForm/RegisterForm.js
+++ client/components/Auth/RegisterForm/RegisterForm.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
 
-export default function RegisterForm({showLoginForm}) {
-    return (
-        <div>
-            <h1>Estamos en el formulario de registro</h1>
-            <button onClick={showLoginForm}>Login</button>
-        </div>
-    )
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+export default function RegisterForm({ showLoginForm }) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
+  return (
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="name"
+        type="text"
+        placeholder="Nombre"
+        onChange={formik.handleChange}
+      />
+      <Form.Input
+        name="lastname"
+        type="text"
+        placeholder="Apellidos"
+        onChange={formik.handleChange}
+      />
+      <Form.Input
+        name="username"
+        type="text"
+        placeholder="Usuario"
+        onChange={formik.handleChange}
+      />
+      <Form.Input
+        name="email"
+        type="text"
+        placeholder="Email"
+        onChange={formik.handleChange}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+      />
+
+      {/* Actions */}
+      <div className="actions">
+        <Button type="button" basic>
+          Iniciar Sesion
+        </Button>
+
+        <Button type="submit" className="submit">
+          Registrar
+        </Button>
+      </div>
+    </Form>
+  );
+}
+
+function initialValues() {
+  return {
+    name: "",
+    lastname: "",
+    username: "",
+    username: "",
+    email: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required("El apellido es obligatorio"),
+  };
 }
